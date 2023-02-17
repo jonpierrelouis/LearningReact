aed23100453f733caa615cee8a89d4659fbce37a
Jonathan Pierre-Louis <jopierrelouis@gmail.com>
2023-02-17 16:20:25 -0500

    added routing

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -5,11 +5,12 @@ import RouteExample from "./components/RouteExample"
 const App = () => {
   return (
     <>
+    {/* Added navbar */}
       <nav>
         <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/ex">Route Example</Link></li>
-        </ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/ex">Route Example</Link></li>
+        </ul> 
       </nav>
 
       <Routes>
